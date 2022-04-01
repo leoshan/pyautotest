4191d325189c89f9d4f0e0360c41132097a5bd21
Leo <leoshch@gmail.com>
2022-04-01 11:34:35 +0800

    Update get_mouse_position.py

--- get_mouse_position.py
+++ get_mouse_position.py
@@ -1,8 +1,11 @@
-import pyautogui
-
+#! python3
+import pyautogui, sys
+print('Press Ctrl-C to quit.')
 try:
     while True:
         x, y = pyautogui.position()
-        print(x, y)
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
 except KeyboardInterrupt:
-    print('\nExit.')
+    print('\n')
