cc158dfe5c029f2e5cf48b532804f4ae5e37ae3f
Leo <leoshch@gmail.com>
2022-04-14 14:44:18 +0800

    Update buy.py

--- buy.py
+++ buy.py
@@ -1,9 +1,11 @@
 import pyautogui
 import time
 
+#exe 60s
+t_end = time.time() + 30
 clickx,clicky = 1020,820
 try:
-    while True:
+    while time.time() < t_end:
         pyautogui.click(clickx,clicky)
 except KeyboardInterrupt:
     print('\nExit.')
